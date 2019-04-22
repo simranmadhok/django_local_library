c32e1989d9b44f4b304af53e0e9c386062d48670
Simran Madhok <simranmadhok@users.noreply.github.com>
2019-04-22 17:33:56 +0530

    Update settings.py

--- Library/settings.py
+++ Library/settings.py
@@ -140,8 +140,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'dolphin2016water@gmail.com'
-EMAIL_HOST_PASSWORD = 'animalyear'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 LOGIN_URL = '/catalog/customer_login/'
 
